7e36d3db7b1c67c62bb173aadba38e2234d3b815
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-09-19 13:38:12 +0300

    experiments with bcrypt-nodejs: https://www.npmjs.com/package/bcrypt-nodejs

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const saltRounds = 10;
+const myPlaintextPassword = 'salasana';
+const someOtherPlaintextPassword = 'not_bacon';
 
 const app = express();
 app.use(express.json());
@@ -21,6 +25,13 @@ const database = {
       entries: 0,
       joined: new Date()
     }
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'john@email.com'
+    }
   ]
 }
 
@@ -29,6 +40,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+  bcrypt.compare("secret", '$2a$10$orAZq29t43vrHNaDl.mve.TH0dp3L0W0IJCtJ7HLPPQ3KPXt0uxeC', function (err, res) {
+    console.log('first guess', res)
+  });
+  bcrypt.compare("secret2", '$2a$10$orAZq29t43vrHNaDl.mve.TH0dp3L0W0IJCtJ7HLPPQ3KPXt0uxeC', function (err, res) {
+    console.log('second guess', res)
+  });
   if (req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password) {
     res.json('success');
@@ -40,6 +57,10 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
+  // bcrypt.hash(password, null, null, function (err, hash) {
+  //   console.log(hash);
+  // });
+
   database.users.push({
     id: '125',
     name: name,
@@ -60,12 +81,12 @@ app.get('/profile/:id', (req, res) => {
       return res.json(user);
     }
   })
-  if(!found){
+  if (!found) {
     res.status(400).json('not found')
   }
 })
 
-app.post('/image', (req,res)=> {
+app.post('/image', (req, res) => {
   const { id } = req.body;
   let found = false;
   database.users.forEach(user => {
@@ -80,6 +101,14 @@ app.post('/image', (req,res)=> {
   }
 })
 
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function (err, res) {
+//   // res == true
+// });
+// bcrypt.compare("veggies", hash, function (err, res) {
+//   // res = false
+// });
 
 app.listen(3000, () => {
   console.log('app is running on port 3000')
