8c56222547b7ff511d054e8460b6f69ea6eb2a0b
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-09-21 14:53:18 +0300

    corrected profile/:id-route to work with postman and front

--- server.js
+++ server.js
@@ -78,16 +78,17 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
+  db.select('*').from('users').where({ id }).then(user => {
+    if (user.length) {
+      res.json(user[0]);
+    } else {
+      res.status(400).json('not found');
     }
   })
-  if (!found) {
-    res.status(400).json('not found')
-  }
+    .catch(err => res.status(400).json('error geting user'));
+  // if (!found) {
+  //   res.status(400).json('not found')
+  // }
 })
 
 app.put('/image', (req, res) => {
