acfb94d68e8b4cf48b5202d369a389ac51367ab2
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-09-23 11:44:01 +0300

    added image- and imageUrl-routes to hide api-key from Newtwork-traffic, corrected profile-route to profile/:id route

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const knex = require('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
-// const image = require('./controllers/image');
+const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
@@ -25,18 +25,9 @@ app.use(cors());
 app.get('/', (req, res) => { res.send(db.users) })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.post('/profile', (req, res) => { profile.handleProfile(req, res, db) })
-
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-      res.json(entries[0])
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
-})
+app.post('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(3000, () => {
   console.log('app is running on port 3000');
