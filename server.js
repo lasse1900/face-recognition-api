949ff22bb8aad3298b2807d564d212afdb03b9a3
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-09-23 14:16:15 +0300

    added database connection

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-vertical-99064',
-    user: 'laurikyttala',
-    password: '',
-    database: 'smart-brain'
+    host: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
